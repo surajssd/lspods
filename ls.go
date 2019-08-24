a763551d313cebb6a920fae2effa52bb87042774
Suraj Deshmukh <surajd.service@gmail.com>
2019-08-24 15:46:14 +0530

    Step6: Create custom pod

--- ls.go
+++ ls.go
@@ -3,25 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
-	"text/tabwriter"
 
-	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
 	argsWithoutProg := os.Args[1:]
-	if len(argsWithoutProg) == 0 {
-		fmt.Printf("no resource mentioned, example command run: \n./ls pods\n")
+	if len(argsWithoutProg) < 1 {
+		fmt.Printf("no resource mentioned, example command run: \n./ls create <podname>\n")
 		os.Exit(-1)
 	}
 
-	k8sresource := argsWithoutProg[0]
-	if k8sresource != "pods" {
-		fmt.Printf("error: the server doesn't have a resource type %q\n", k8sresource)
-		os.Exit(-1)
-	}
+	podName := argsWithoutProg[1]
 
 	kubeconfig := getKubeConfig()
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
@@ -36,18 +32,30 @@ func main() {
 		os.Exit(-1)
 	}
 
-	ns := "kube-system"
-	pods, err := cs.CoreV1().Pods(ns).List(meta_v1.ListOptions{})
-	if err != nil {
-		fmt.Printf("could not get pods in %q namespace: %v\n", ns, err)
+	ns := "default"
+	pod := &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      podName,
+			Namespace: ns,
+			Labels: map[string]string{
+				"app": "mypod",
+			},
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:  podName,
+					Image: "bitnami/nginx",
+				},
+			},
+		},
 	}
 
-	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
-	fmt.Fprintln(w, "NAME\tIP\tNODE")
-	for _, pod := range pods.Items {
-		fmt.Fprintf(w, "%s\t%s\t%s\n", pod.Name, pod.Status.PodIP, pod.Spec.NodeName)
+	_, err = cs.CoreV1().Pods(ns).Create(pod)
+	if err != nil {
+		fmt.Printf("could not create pods in %q namespace: %v\n", ns, err)
 	}
-	w.Flush()
+	fmt.Println("Created pod", podName, "successfully")
 }
 
 func getKubeConfig() string {
